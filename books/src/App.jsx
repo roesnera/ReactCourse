eaca5f09be1f4be466345ad9e60d830b7b7d9d8b
roesnera <adam.roesner@generation.org>
2023-04-29 17:24:04 -0400

    modifies books project to make db requests using json-server

--- books/src/App.jsx
+++ books/src/App.jsx
@@ -1,44 +1,64 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import BookCreate from './components/BookCreate';
 import BookList from './components/BookList';
-import "./index.css"
+import "./index.css";
+import axios from "axios"
 
 function App() {
   const [books, setBooks] = useState([]);
 
-  const createBook = (title) => {
-    const newBookArray = [
-      ...books,
-      { id: Math.round(Math.random()*99999),
-        title
-      }
-    ]
-    setBooks(newBookArray);
+  const fetchBooks = async () => {
+    const newBooks = await axios("http://localhost:3001/books");
+    setBooks(newBooks.data);
   }
 
-  const editBook = ({ id, title }) => {
+  useEffect(() => {
+    fetchBooks();
+  }, [])
 
-    const newBooks = books.map((book) => {
-      if(book.id === id){
-        return {...book, title}
-      }
-      return book;
-    })
+  const createBook = async (title) => {
+    
+    await axios.post("http://localhost:3001/books", {title});
+    
+    // console.log(newBook);
 
-    setBooks(newBooks);
+    await fetchBooks();
+  }
+
+  const editBook = async ({ id, title }) => {
+
+    await axios.put(`http://localhost:3001/books/${id}`, {title});
+
+    await fetchBooks();
+
+
+    // const newBooks = books.map((book) => {
+    //   if(book.id === id){
+    //     return {...book, title}
+    //   }
+    //   return book;
+    // })
+
+    // setBooks(newBooks);
     // do something to edit books array with new book
   }
 
-  const deleteBook = (bookId) => {
-    // console.log(`deleting ${bookId}`);
+  const deleteBook = async (bookId) => {
+    console.log(`deleting ${bookId}`);
 
-    const newBooks = books.filter((element) => element.id!==bookId);
+    await axios.delete(`http://localhost:3001/books/${bookId}`);
+
+    // console.log(deletedBook.data);
+
+    await fetchBooks();
+
+    // const newBooks = books.filter((element) => element.id!==bookId);
 
     // console.log(newBooks)
 
     //TODO: incomplete funciton, determine logic for deleteBook
-    setBooks(newBooks);
+    // setBooks(newBooks);
   }
 
   return (
