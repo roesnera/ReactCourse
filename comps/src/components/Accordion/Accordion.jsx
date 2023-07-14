345e63cb0a6b7abf829b3733cef20f2867d9e5be
roesnera <adam.roesner@generation.org>
2023-07-14 16:23:07 -0400

    adds some notes on functional state updating

--- comps/src/components/Accordion/Accordion.jsx
+++ comps/src/components/Accordion/Accordion.jsx
@@ -1,22 +1,43 @@
 // import React from 'react';
 import './Accordion.css';
 import { useState } from 'react';
+import { GoChevronDown, GoChevronLeft } from 'react-icons/go';
 
 export default function Accordion({ accordionItems }) {
-  const [expandedIndex, setExpandedIndex] = useState(0);
+  const [expandedIndex, setExpandedIndex] = useState(-1);
+
+  function handleClick(index) {
+    // this function will use the actual most up to date value for the expanded index
+    // this will prevent any bugs from programmatic site interaction that queues changes to state
+    // faster than the React DOM actually updates the real dom
+    // curent index is the live value of expanded index regardless of rendering
+    // whereas you could say that index is the stale value of expanded index
+    // this function will update expanded index to -1 if the live value is the same as the value passed in
+    // otherwise it will update expanded index to the new value
+    setExpandedIndex((currentIndex)=>{
+      if(currentIndex===index) return -1;
+      else return index;
+    });
+  }
 
   const renderedItems = accordionItems.map((item, index) => {
       const isExpanded = index === expandedIndex;
+
+      const icon = (
+        <span>{isExpanded? <GoChevronDown />: <GoChevronLeft />}</span>
+      )
+
       return (
         <div key={item.id}>
-          <div onClick={() => setExpandedIndex(index)}>
+          <div onClick={() => handleClick(index)} className='flex justify-between p-3 bg-gray-50 border-b items-center cursor-pointer'>
             {item.label}
+            {icon}
           </div>
-          {isExpanded && <div> {item.content}</div>}
+          {isExpanded && <div className='border-b p-5'> {item.content}</div>}
         </div>
       )
   })
 
 
-  return <div>{renderedItems}</div>;
+  return <div className='border-x border-t rounded'>{renderedItems}</div>;
 }
